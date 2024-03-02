ec76f9630f0c2f48e68f2fe89fb4ac3e7e752be7
Alexander <alexanderher@vmware.com>
2024-03-02 18:19:35 +0100

    [TASK-ID] iac: export just the outputs used by the modules in the main repository

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
 output "hpa_name" {
   description = "The name of the deployed HPA."
-  value       = kubernetes_horizontal_pod_autoscaler.main.metadata[0].name
+  value       = kubernetes_horizontal_pod_autoscaler_v1.hpa.metadata.0.name
+}
+
+output "ingress" {
+  description = "All the ingress values."
+  value       = kubernetes_ingress_v1.ingress
+}
+
+output "persistent_volume_claim_name" {
+  description = "The persistent volume claim name."
+  value       = kubernetes_persistent_volume_claim_v1.pgdata.0.metadata.0.name
+}
+
+output "service_account_name" {
+  description = "The name of the service account."
+  value       = kubernetes_service_account_v1.service_account.metadata.0.name
 }
